500bc0fc6632cefadf17652b20505cff37a47344
kamiye <kamiye31@gmail.com>
2025-07-23 16:06:28 +0100

    feat: update metadata for portfolio and enhance layout styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <div>
+      <div className="flex justify-center items-center">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </div>
     </main>
