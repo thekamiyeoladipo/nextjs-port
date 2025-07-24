f8a352ec9e39af2da30e9e9183e7099328ea1a37
kamiye <kamiye31@gmail.com>
2025-07-24 19:52:35 +0100

    feat: grid background bug successfully fixed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <Spotlight />
       </div>
 
-      {/* <div
+      <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
         absolute top-0 left-0 flex items-center justify-center"
       >
@@ -16,9 +16,9 @@ const Hero = () => {
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
           bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
-      </div> */}
+      </div>
 
-      <div className="flex justify-center">
+      <div className="relative flex justify-center">
         <div className="max-w-[89vw] md:max-w-2xl
         lg:max-w-[60vw] flex flex-col items-center
         justify-center">
