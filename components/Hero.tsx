d75dc936d459424eba607965dab3487e1b43ce4d
kamiye <kamiye31@gmail.com>
2025-07-24 19:16:49 +0100

    feat: add ThemeProvider component and integrate it into RootLayout; update package.json and package-lock.json to include next-themes dependency

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,11 +4,26 @@ import { Spotlight } from "./ui/spotlight-new"
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-        <div>
-            <Spotlight />
-        </div>
-    </div>
-  )
-}
+    
+      <div>
+        <Spotlight />
+      </div>
 
-export default Hero
+    
+      <div
+        className="h-screen w-full dark:bg-black bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+        absolute top-0 left-0 flex items-center justify-center"
+      >
+    
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black
+          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+
+     
+    </div>
+  );
+};
+
+export default Hero;
