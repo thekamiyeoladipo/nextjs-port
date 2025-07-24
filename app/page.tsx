951ef5f93812b0c1907a7b006a0897803419c6bf
kamiye <kamiye31@gmail.com>
2025-07-24 14:13:10 +0100

    feat: replace placeholder text with Hero component in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "@/components/Hero";
+
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center
     items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1 className="text-white">Hello world!</h1>
+        <Hero />
       </div>
     </main>
   );
