4a5302ffae52a74b5e50e79c8d3bdc3e9a896ae5
kamiye <kamiye31@gmail.com>
2025-07-24 14:25:34 +0100

    feat: update Home component layout and enhance Hero component structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Hero from "@/components/Hero";
 
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center
+    <main className="relative bg-black flex justify-center
     items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        {/* <h2 className="text-white">Hello World</h2> */}
         <Hero />
       </div>
     </main>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+
 
 const Hero = () => {
   return (
-    <div>
-        
+    <div className="pb-20 pt-36">
+        <div>
+            
+        </div>
     </div>
   )
 }
